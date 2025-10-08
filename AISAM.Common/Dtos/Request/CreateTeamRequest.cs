785d858b04ac4029bb2a5a3e5c79a69561cf4b80
Ngochieu3165 <163821440+Ngochieu3165@users.noreply.github.com>
2025-10-09 01:25:10 +0700

    Update Validation Team
    Update Validation Team

--- AISAM.Common/Dtos/Request/CreateTeamRequest.cs
+++ AISAM.Common/Dtos/Request/CreateTeamRequest.cs
@@ -3,8 +3,11 @@ namespace AISAM.Common.Dtos.Request
     public class CreateTeamRequest
     {
         // Admin may specify VendorId; Vendors cannot override and it will be set to current user
+        [Required]
         public Guid? VendorId { get; set; }
+        [MaxLength(255)]
         public string Name { get; set; } = string.Empty;
+        [MaxLength(2000)]
         public string? Description { get; set; }
     }
 }
--- AISAM.Common/Dtos/Request/UpdateTeamRequest.cs
+++ AISAM.Common/Dtos/Request/UpdateTeamRequest.cs
@@ -2,7 +2,9 @@ namespace AISAM.Common.Dtos.Request
 {
     public class UpdateTeamRequest
     {
+        [MaxLength(255)]
         public string Name { get; set; } = string.Empty;
+        [MaxLength(2000)]
         public string? Description { get; set; }
     }
 }
